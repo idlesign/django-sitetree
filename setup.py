f1759703bb50c866e5337629a8a0c66b004474b1
idle sign <idlesign@yandex.ru>
2013-02-27 20:59:15 +0700

    README is explicitly marked as .rst.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 from sitetree import VERSION
 
-f = open(os.path.join(os.path.dirname(__file__), 'README'))
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
 f.close()
 
