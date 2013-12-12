a1760f568635215d2987ec531bee8647ffe86063
idle sign <idlesign@yandex.ru>
2013-12-12 22:27:25 +0700

    Added Django 1.6 test rules.

--- sitetree/runtests.py
+++ sitetree/runtests.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python
+import sys
+import os
+
 from django.conf import settings
-from django.core.management import call_command
 
 
-if not settings.configured:
-    settings.configure(
-        INSTALLED_APPS=('django.contrib.auth', 'django.contrib.contenttypes', 'sitetree'),
-        DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
-    )
+APP_NAME = 'sitetree'
+
+
+def main():
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    if not settings.configured:
+        settings.configure(
+            INSTALLED_APPS=('django.contrib.auth', 'django.contrib.contenttypes', APP_NAME),
+            DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
+        )
+
+    from django.test.utils import get_runner
+    runner = get_runner(settings)()
+    failures = runner.run_tests((APP_NAME,))
+
+    sys.exit(failures)
+
 
 if __name__ == '__main__':
-    call_command('test', 'sitetree')
+    main()
