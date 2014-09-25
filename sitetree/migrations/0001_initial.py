6eb885c028f713df3a3100672fbc22ec693c07fe
idle sign <idlesign@yandex.ru>
2014-09-25 09:35:42 +0700

    Added Django 1.7 migrations.

--- sitetree/migrations/0001_initial.py
+++ sitetree/migrations/0001_initial.py
@@ -1,78 +1,63 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Tree'
-        db.create_table('sitetree_tree', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('alias', self.gf('django.db.models.fields.CharField')(unique=True, max_length=80, db_index=True)),
-        ))
-        db.send_create_signal('sitetree', ['Tree'])
-
-        # Adding model 'TreeItem'
-        db.create_table('sitetree_treeitem', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('hint', self.gf('django.db.models.fields.CharField')(default='', max_length=200, blank=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=200, db_index=True)),
-            ('urlaspattern', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
-            ('tree', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sitetree.Tree'])),
-            ('hidden', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
-            ('alias', self.gf('sitetree.models.CharFieldNullable')(db_index=True, max_length=80, null=True, blank=True)),
-            ('description', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
-            ('inmenu', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
-            ('inbreadcrumbs', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
-            ('insitetree', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
-            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sitetree.TreeItem'], null=True, blank=True)),
-            ('sort_order', self.gf('django.db.models.fields.IntegerField')(default=0, db_index=True)),
-        ))
-        db.send_create_signal('sitetree', ['TreeItem'])
-
-        # Adding unique constraint on 'TreeItem', fields ['tree', 'alias']
-        db.create_unique('sitetree_treeitem', ['tree_id', 'alias'])
+from django.db import models, migrations
+import sitetree.models
 
 
-    def backwards(self, orm):
-        
-        # Removing unique constraint on 'TreeItem', fields ['tree', 'alias']
-        db.delete_unique('sitetree_treeitem', ['tree_id', 'alias'])
+class Migration(migrations.Migration):
 
-        # Deleting model 'Tree'
-        db.delete_table('sitetree_tree')
+    dependencies = [
+        ('auth', '0001_initial'),
+    ]
 
-        # Deleting model 'TreeItem'
-        db.delete_table('sitetree_treeitem')
-
-
-    models = {
-        'sitetree.tree': {
-            'Meta': {'object_name': 'Tree'},
-            'alias': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80', 'db_index': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        },
-        'sitetree.treeitem': {
-            'Meta': {'unique_together': "(('tree', 'alias'),)", 'object_name': 'TreeItem'},
-            'alias': ('sitetree.models.CharFieldNullable', [], {'db_index': 'True', 'max_length': '80', 'null': 'True', 'blank': 'True'}),
-            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
-            'hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
-            'hint': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'inbreadcrumbs': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
-            'inmenu': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
-            'insitetree': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sitetree.TreeItem']", 'null': 'True', 'blank': 'True'}),
-            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'tree': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sitetree.Tree']"}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_index': 'True'}),
-            'urlaspattern': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'})
-        }
-    }
-
-    complete_apps = ['sitetree']
+    operations = [
+        migrations.CreateModel(
+            name='Tree',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(help_text='Site tree title for presentational purposes.', max_length=100, verbose_name='Title', blank=True)),
+                ('alias', models.CharField(help_text='Short name to address site tree from templates.<br /><b>Note:</b> change with care.', unique=True, max_length=80, verbose_name='Alias', db_index=True)),
+            ],
+            options={
+                'abstract': False,
+                'verbose_name': 'Site Tree',
+                'verbose_name_plural': 'Site Trees',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TreeItem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(help_text='Site tree item title. Can contain template variables E.g.: {{ mytitle }}.', max_length=100, verbose_name='Title')),
+                ('hint', models.CharField(default=b'', help_text='Some additional information about this item that is used as a hint.', max_length=200, verbose_name='Hint', blank=True)),
+                ('url', models.CharField(help_text='Exact URL or URL pattern (see "Additional settings") for this item.', max_length=200, verbose_name='URL', db_index=True)),
+                ('urlaspattern', models.BooleanField(default=False, help_text='Whether the given URL should be treated as a pattern.<br /><b>Note:</b> Refer to Django "URL dispatcher" documentation (e.g. "Naming URL patterns" part).', db_index=True, verbose_name='URL as Pattern')),
+                ('hidden', models.BooleanField(default=False, help_text='Whether to show this item in navigation.', db_index=True, verbose_name='Hidden')),
+                ('alias', sitetree.models.CharFieldNullable(max_length=80, blank=True, help_text='Short name to address site tree item from a template.<br /><b>Reserved aliases:</b> "trunk", "this-children", "this-siblings", "this-ancestor-children", "this-parent-siblings".', null=True, verbose_name='Alias', db_index=True)),
+                ('description', models.TextField(default=b'', help_text='Additional comments on this item.', verbose_name='Description', blank=True)),
+                ('inmenu', models.BooleanField(default=True, help_text='Whether to show this item in a menu.', db_index=True, verbose_name='Show in menu')),
+                ('inbreadcrumbs', models.BooleanField(default=True, help_text='Whether to show this item in a breadcrumb path.', db_index=True, verbose_name='Show in breadcrumb path')),
+                ('insitetree', models.BooleanField(default=True, help_text='Whether to show this item in a site tree.', db_index=True, verbose_name='Show in site tree')),
+                ('access_loggedin', models.BooleanField(default=False, help_text='Check it to grant access to this item to authenticated users only.', db_index=True, verbose_name='Logged in only')),
+                ('access_guest', models.BooleanField(default=False, help_text='Check it to grant access to this item to guests only.', db_index=True, verbose_name='Guests only')),
+                ('access_restricted', models.BooleanField(default=False, help_text='Check it to restrict user access to this item, using Django permissions system.', db_index=True, verbose_name='Restrict access to permissions')),
+                ('access_perm_type', models.IntegerField(default=1, help_text='<b>Any</b> &mdash; user should have any of chosen permissions. <b>All</b> &mdash; user should have all chosen permissions.', verbose_name='Permissions interpretation', choices=[(1, 'Any'), (2, 'All')])),
+                ('sort_order', models.IntegerField(default=0, help_text='Item position among other site tree items under the same parent.', verbose_name='Sort order', db_index=True)),
+                ('access_permissions', models.ManyToManyField(to='auth.Permission', verbose_name='Permissions granting access', blank=True)),
+                ('parent', models.ForeignKey(related_name=b'treeitem_parent', blank=True, to='sitetree.TreeItem', help_text='Parent site tree item.', null=True, verbose_name='Parent')),
+                ('tree', models.ForeignKey(related_name=b'treeitem_tree', verbose_name='Site Tree', to='sitetree.Tree', help_text='Site tree this item belongs to.')),
+            ],
+            options={
+                'abstract': False,
+                'verbose_name': 'Site Tree Item',
+                'verbose_name_plural': 'Site Tree Items',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='treeitem',
+            unique_together=set([('tree', 'alias')]),
+        ),
+    ]
