ef5170ac7809d1692ca9e2596a18d91e463059dc
wlorenzetti <lorenzetti@gis3w.it>
2016-02-15 10:00:32 +0100

    Add custom fields to tree utils function for custom Tree model.

--- sitetree/utils.py
+++ sitetree/utils.py
@@ -24,12 +24,12 @@ def generate_id_for(obj):
     return id(obj)
 
 
-def tree(alias, title='', items=None):
+def tree(alias, title='', items=None, **kwargs):
     """Dynamically creates and returns a sitetree.
     `items` - dynamic sitetree items objects created by `item` function.
 
     """
-    tree_obj = get_tree_model()(alias=alias, title=title)
+    tree_obj = get_tree_model()(alias=alias, title=title, **kwargs)
     tree_obj.id = generate_id_for(tree_obj)
     tree_obj.is_dynamic = True
 
