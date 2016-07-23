bb450bd8a555ebf802b07e94b7594d9cff623c35
idle sign <idlesign@yandex.ru>
2016-07-23 19:33:30 +0600

    Year updated.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-sitetree'
-copyright = u'2011-2015, Igor \'idle sign\' Starikov'
+copyright = u'2011-2016, Igor \'idle sign\' Starikov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
