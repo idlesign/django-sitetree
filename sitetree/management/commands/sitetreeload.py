11ed9c0d10483aab620e188d9b84ce8e1d5b409b
idle sign <idlesign@yandex.ru>
2016-12-21 23:33:26 +0700

    sitetreeload command fix

--- sitetree/management/commands/sitetreeload.py
+++ sitetree/management/commands/sitetreeload.py
@@ -30,7 +30,7 @@ get_options = options_getter((
         help='Mode to put data into DB. Variants: `replace`, `append`.'),
 
     CommandOption(
-        '--items_into_tree', action='store', dest='into_tree', default=None,
+        '--items_into_tree', action='store', dest='items_into_tree', default=None,
         help='Import only tree items data into tree with given alias.'),
 ))
 
@@ -50,7 +50,7 @@ class Command(BaseCommand):
 
         using = options.get('database', DEFAULT_DB_ALIAS)
         mode = options.get('mode', 'append')
-        items_into_tree = options.get('into_tree', None)
+        items_into_tree = options.get('items_into_tree', None)
 
         if items_into_tree is not None:
             try:
