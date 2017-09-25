ad2cc2499b91046cf2841214d4ff269bc4a346e0
stop5 <stop5@users.noreply.github.com>
2017-09-25 12:56:30 +0200

    fixed my error
    replaced django.conf.urls with django.core.urlresolvers.

--- sitetree/admin.py
+++ sitetree/admin.py
@@ -3,7 +3,7 @@ from django import VERSION as django_version
 try:
     from django.urls import get_urlconf, get_resolver
 except ImportError:
-    from django.conf.urls import get_urlconf, get_resolver
+    from django.core.urlresolvers import get_urlconf, get_resolver
 from django.utils.translation import ugettext_lazy as _
 from django.utils import six
 from django.http import HttpResponseRedirect
