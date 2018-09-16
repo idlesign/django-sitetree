fb9bf4f201227213e989fb93ce5f19ce95b6d2b7
idle sign <idlesign@yandex.ru>
2018-09-16 10:09:15 +0700

    Demo Django 2 compatibility.

--- demo/settings/urls.py
+++ demo/settings/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
-from demo import urls as demo_urls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include(demo_urls, namespace='demo')),
+    url(r'', include(('demo.urls', 'demo'), namespace='demo')),
 ]
 
 
