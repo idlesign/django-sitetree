4e9b1bc53097926d7071943dc15b02850efda0d1
idle sign <idlesign@yandex.ru>
2021-12-18 13:16:52 +0700

    Import fixes.

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import index, listing, detailed
 
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^articles/$', listing, name='articles-listing'),
-    url(r'^articles/(?P<article_id>\d+)/$', detailed, name='articles-detailed'),
+    re_path(r'^$', index, name='index'),
+    re_path(r'^articles/$', listing, name='articles-listing'),
+    re_path(r'^articles/(?P<article_id>\d+)/$', detailed, name='articles-detailed'),
 ]
--- demo/settings/urls.py
+++ demo/settings/urls.py
@@ -1,16 +1,16 @@
 from django.conf import settings
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include(('demo.urls', 'demo'), namespace='demo')),
+    path('admin/', admin.site.urls),
+    path('', include(('demo.urls', 'demo'), namespace='demo')),
 ]
 
 
 if settings.DEBUG and settings.USE_DEBUG_TOOLBAR:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
